dc82b32ed360d995e7a056c18d0276f57bb3edfa
Taylor Ng <taylormng95@gmail.com>
2024-07-19 20:37:38 -0400

    completes lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,19 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    return make_response([article.to_dict() for article in articles], 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    if session.get('page_views') == None:
+        session['page_views'] = 0
+    session['page_views'] += 1
+    if session.get('page_views') <= 3:
+        article = Article.query.filter(Article.id == id).first()
+        return make_response(article.to_dict(), 200)
+    else:
+        return make_response({'message': 'Maximum pageview limit reached'}, 401)
 
 if __name__ == '__main__':
     app.run(port=5555)
